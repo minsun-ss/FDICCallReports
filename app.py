f6c7d581a763c45005cba393e09a1fda8b35fa4d
stuffofminsun <metokki@gmail.com>
2019-04-22 15:42:19 -0400

    fixed the data and updated the total charts to update as well

--- app.py
+++ app.py
@@ -7,61 +7,60 @@ from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 
 munidata = pd.read_csv('munidata.csv').set_index('Financial Institution Name')
-munidata.sort_values(by='2001Q1', ascending=True, inplace=True)
+munitotal = pd.read_csv('munitotal.csv').set_index('CallDate')
 
-# build the dict for the dropdowns
-dropdown = [{'label': i, 'value': i} for i in munidata.columns.values]
-
-# cycle through the location of the quarter
-current_quarter_location = 0
+# build the dict for the title name
+quarter_names = {'Q1': 'First Quarter', 'Q2': 'Second Quarter', 'Q3': 'Third Quarter', 'Q4': 'Fourth Quarter'}
 
 # launch app
 app = dash.Dash(__name__)
-app.title = 'Munis!!!!'
+app.title = 'Municipal Loans Held By Banks'
 server = app.server
 
 def serve_layout():
     return html.Div(children=[
-            html.H5(children='Municipal Loans'),
-
+            html.H5(children='Municipal Loans', id='graph_title'),
             html.Button('Press Here to Advance a Quarter', id='keypress'),
+            dcc.Graph(id='muni_total_graph', ),
+
 
             dcc.Graph(
                 id='muni_graph',
             ),
-        ])
 
+            html.Div(id='storage_value', style={'display': 'none'})
+        ])
 
 app.layout = serve_layout
 
+@app.callback(
+    Output('storage_value', 'children'),
+    [Input('keypress', 'n_clicks')]
+)
+def update_data(quarter_update):
+    return quarter_update
+
 # the interactive section
 @app.callback(
-    [Output('muni_graph', 'figure')],
-##    [Input('quarter_menu', 'value'),
-     [Input('keypress', 'n_clicks')]
-)
-def update_figure(selected_press):
-    global current_quarter_location
-    current_quarter = munidata.columns.values[current_quarter_location]
+    [Output('muni_graph', 'figure'), Output('muni_total_graph', 'figure'), Output('graph_title', 'children')],
+    [Input('keypress', 'n_clicks'),
+     Input('storage_value', 'children')
+     ])
+def update_figure(selected_press, storage_value):
 
-    # this moves to the next one in the stack but isn't used
-    current_quarter_location += 1
+    # determines quarter based on stored value
+    if storage_value is None:
+        current_quarter_loc = 0
+    else: current_quarter_loc = storage_value % len(munidata.columns.values)
 
-    if current_quarter_location == len(munidata.columns.values):
-        current_quarter_location = 0
-
-    # check to see if selected_asset is empty
-    #selected_quarter = selected_asset
-    #if selected_quarter is None:
-    #    selected_quarter = '2001Q1'
+    current_quarter = munidata.columns.values[current_quarter_loc]
 
     # re-sort the muni values and maybe just get top 50
     munidata.sort_values(by=current_quarter, ascending=True, inplace=True)
     subset = munidata.tail(50).copy()
 
-    # am not sure why the hell this needs ot be returned as a tuple/list instead of just a dict like in the previous
-    # visualization, but.... well, whatever works
-    a_figure=[{
+    # build the top 50 bar
+    bar_figure={
             'data': [
                 go.Bar(
                     y=subset.index,
@@ -72,33 +71,48 @@ def update_figure(selected_press):
                 ),
             ],
             'layout': go.Layout(
-                title= go.layout.Title(text=current_quarter,x=.2),
                 height=900,
-                xaxis={'mirror': 'allticks', 'side': 'top', 'range': [0, 5], 'type': 'log', 'title': go.layout.xaxis.Title(
-                    text='$ millions'
-                )},
+                xaxis={'mirror': 'allticks', 'side': 'top', 'range': [0, 25000],
+                       'title': go.layout.xaxis.Title(text='$ millions'),},
                 margin=go.layout.Margin(
                     l=400,
                     t=40
                 ),
             )
-        }]
+        }
 
-    return a_figure
+    # set all colors to gray, then highlight the approprate color
+    munitotal['Color'] = 'rgba(204,204,204,1)'
+    munitotal.loc[current_quarter, 'Color'] = 'rgba(222,45,38,0.8)'
 
-'''        dcc.Dropdown(
-        options=dropdown,
-        id='quarter_menu',
-        placeholder='Current Quarter',
-        clearable='False',
-        searchable='True'
-    ), 
-    dcc.Slider(
-        id = 'quarter_slider',
-        min = 0,
-        max = len(munidata.columns.values)-1,
-        marks = {int(np.where(munidata.columns.values == i)[0][0]): i for i in munidata.columns.values}
-    ),'''
+    # build the total bar
+    total_figure = {
+        'data': [go.Bar(
+        y=munitotal['ALL BANKS'].values,
+        x=munitotal.index,
+        name='All',
+        hoverinfo='skip',
+        marker=dict(
+            color=munitotal['Color'].values
+        ),
+        ),],
+        'layout': go.Layout(
+            height=150,
+            margin=go.layout.Margin(
+                l=400,
+                t=40),
+            xaxis=dict(
+                ticklen=8
+            )
+        )
+    }
+
+    new_title='Municipal Loans by Top 50 Banks: {} {}'.format(quarter_names[current_quarter[-2:]], current_quarter[:4])
+    print(current_quarter[-2:])
+
+    return bar_figure, total_figure, new_title
+
+
 
 
 
